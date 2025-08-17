35f7eb7d00b507d894476fa9e8d567ffad7842b0
soler2000 <awilde76@gmail.com>
2025-08-17 23:35:23 +0100

    Create graceful_shutdown.py

---
+++ bin/graceful_shutdown.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import time, subprocess
+from sensors.ups_ina219 import UpsMonitor
+
+ups = UpsMonitor(addr=0x43, busnum=1)
+THRESH = 3.3  # volts; override via DB in main app normally
+LOW_COUNT = 0
+while True:
+    snap = ups.snapshot()
+    if snap["voltage"] <= THRESH:
+        LOW_COUNT += 1
+    else:
+        LOW_COUNT = 0
+    if LOW_COUNT >= 5:  # ~10s debounce
+        subprocess.call(["systemctl","poweroff"])
+        break
+    time.sleep(2)
