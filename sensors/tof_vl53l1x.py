5768036c877571f81e2493e79fd460a4ed07b222
soler2000 <awilde76@gmail.com>
2025-08-17 23:45:19 +0100

    Create tof_vl53l1x.py

---
+++ sensors/tof_vl53l1x.py
@@ -0,0 +1,11 @@
+import board, busio, adafruit_vl53l1x, os
+class ToFSensor:
+    def __init__(self, addr=0x29, busnum=1):
+        self.i2c = busio.I2C(getattr(board, f"SCL{busnum-1}"), getattr(board, f"SDA{busnum-1}"))
+        self.vl = adafruit_vl53l1x.VL53L1X(self.i2c, address=addr)
+        self.vl.distance_mode = 2  # long
+        self.vl.timing_budget = 50
+        self.vl.start_ranging()
+    def distance_m(self):
+        d_mm = self.vl.distance
+        return (d_mm/1000.0) if d_mm is not None else None
